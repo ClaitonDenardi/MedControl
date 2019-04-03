5d9215fd2626b6cf7043ad4d94da096844405f74
ClaitonDenardi <cdpaulus@inf.ufsm.br>
2019-04-03 14:49:45 -0300

    6 commit

--- app/src/main/java/cdpaulus/medcontrol/AdicionarMedicamento.java
+++ app/src/main/java/cdpaulus/medcontrol/AdicionarMedicamento.java
@@ -6,26 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
 public class AdicionarMedicamento extends Activity {
 
     public static TextView resultExtView;
-    Button scan_btn;
+    private Button scan_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_medicamento);
-
-        resultExtView = (TextView)findViewById(R.id.btn_scan);
+        scan_btn = findViewById(R.id.btn_scan);
         scan_btn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ScanCodeActivity.class);
-                startActivity(intent);
+                OpenReader();
             }
         });
+
     }
+    private void OpenReader(){
+        Intent intent = new Intent(getApplicationContext(), ScanCodeActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/cdpaulus/medcontrol/MainActivity.java
+++ app/src/main/java/cdpaulus/medcontrol/MainActivity.java
@@ -1,16 +1,30 @@
 package cdpaulus.medcontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn_adicionar_medicamento = (Button) findViewById(R.id.btn_adicionar_medicamento);
+        btn = findViewById(R.id.btn_adicionar_medicamento);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                OpenActivityAdicionar();
+            }
+        });
+
     }
+    private void OpenActivityAdicionar(){
+        Intent intent= new Intent(MainActivity.this, AdicionarMedicamento.class);
+        startActivity(intent);
+    }
+
 
 }
